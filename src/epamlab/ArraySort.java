3b2aa5c4155bd0eb78cc3586153c84bfa6845421
mirror <mironosk@gmail.com>
2016-04-11 15:58:49 +0300

    new

---
+++ src/epamlab/ArraySort.java
@@ -0,0 +1,17 @@
+package epamlab;
+
+import java.util.Arrays;
+
+public class ArraySort<T extends Comparable<T>> implements Runnable {
+
+	public ArraySort(T[] t) {
+		this.t = t;
+	}
+
+	T[] t;
+
+	@Override
+	public void run() {
+		Arrays.sort(t);
+	}
+}
---
+++ src/epamlab/ParallelSort.java
@@ -0,0 +1,12 @@
+package epamlab;
+
+public class ParallelSort {
+	private static final int core = Runtime.getRuntime().availableProcessors();
+	
+	
+	public void sort(int[] array){
+		
+		
+	}
+
+}
